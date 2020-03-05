41d169620bddea0c418a059aed0964427a642f4a
Anton Frattaroli (afrattar) <afrattar@cisco.com>
2020-03-05 14:34:43 -0600

    feat: add text input component

---
+++ framework/components/ATextInput/ATextInput.js
@@ -0,0 +1,45 @@
+import PropTypes from "prop-types";
+import React, {forwardRef} from "react";
+
+import "./ATextInput.scss";
+
+const ATextInput = forwardRef(
+  (
+    {
+      className: inputClassName,
+      type = "text",
+      validationState = "success",
+      ...rest
+    },
+    ref
+  ) => {
+    let className = "a-text-input";
+    if (validationState !== "success") {
+      className += " a-text-input--state-" + validationState;
+    }
+
+    if (inputClassName) {
+      className += ` ${inputClassName}`;
+    }
+
+    return <input {...rest} ref={ref} type={type} className={className} />;
+  }
+);
+
+ATextInput.defaultProps = {
+  type: "text",
+  validationState: "default"
+};
+
+ATextInput.propTypes = {
+  /**
+   * Change the input type to take advantage of native behavior.
+   */
+  type: PropTypes.oneOf("text", "password", "email"),
+  /**
+   * Applies a validation state.
+   */
+  validationState: PropTypes.oneOf("default", "warning", "danger")
+};
+
+export default ATextInput;
