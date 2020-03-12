96b21d39bd55fe60a271d00fb67c0684c3e55878
Anton Frattaroli (afrattar) <afrattar@cisco.com>
2020-03-12 13:27:36 -0500

    feat: add radio component

---
+++ framework/components/ARadio/ARadio.js
@@ -0,0 +1,82 @@
+import PropTypes from "prop-types";
+import React, {forwardRef} from "react";
+
+import "./ARadio.scss";
+
+const ARadio = forwardRef(
+  (
+    {
+      checked = false,
+      children,
+      className: propsClassName,
+      disabled = false,
+      name,
+      onClick,
+      value,
+      wrap,
+      ...rest
+    },
+    ref
+  ) => {
+    const className = "a-radio";
+
+    if (propsClassName) {
+      className += ` ${propsClassName}`;
+    }
+
+    return (
+      <label {...rest} ref={ref} className={className}>
+        <input
+          type="radio"
+          name={name}
+          className="a-radio__input"
+          value={value}
+          checked={checked}
+          onChange={() => {}}
+          disabled={disabled}
+          onClick={onClick}
+          aria-checked={checked}
+          role="radio"
+        />
+        <span className="a-radio__box" />
+        <span className={`a-radio__label ${wrap && "a-radio__label--wrap"}`}>
+          {children}
+        </span>
+      </label>
+    );
+  }
+);
+
+ARadio.defaultProps = {
+  checked: false,
+  disabled: false
+};
+
+ARadio.propTypes = {
+  /**
+   * Toggles the `checked` state.
+   */
+  checked: PropTypes.bool,
+  /**
+   * Toggles the `disabled` state.
+   */
+  disabled: PropTypes.bool,
+  /**
+   * The input's name.
+   */
+  name: PropTypes.string,
+  /**
+   * A callback for handling the click event.
+   */
+  onClick: PropTypes.func,
+  /**
+   * The input's value.
+   */
+  value: PropTypes.string,
+  /**
+   * Toggles wrapping of long text in the label.
+   */
+  wrap: PropTypes.bool
+};
+
+export default ARadio;
