60fd768f5d46335bbadb5569fe25d7d81308959c
Anton Frattaroli (afrattar) <afrattar@cisco.com>
2020-03-25 09:40:57 -0500

    feat: add layout components

---
+++ framework/components/ALayout/AContainer.js
@@ -0,0 +1,29 @@
+import PropTypes from "prop-types";
+import React, {forwardRef} from "react";
+
+import "./ALayout.scss";
+
+const AContainer = forwardRef(
+  ({className: propsClassName, fluid = false, ...rest}, ref) => {
+    let className = "a-container";
+
+    if (fluid) {
+      className += " a-container--fluid";
+    }
+
+    if (propsClassName) {
+      className += ` ${propsClassName}`;
+    }
+
+    return <div {...rest} ref={ref} className={className} />;
+  }
+);
+
+AContainer.propTypes = {
+  /**
+   * Removes the maximum width viewpoint restriction.
+   */
+  fluid: PropTypes.bool
+};
+
+export default AContainer;
