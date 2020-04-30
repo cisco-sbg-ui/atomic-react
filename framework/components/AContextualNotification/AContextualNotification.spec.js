28ad43c54c1d5d37956e9fdb2e68678f59523962
Anton Frattaroli (afrattar) <afrattar@cisco.com>
2020-04-30 14:32:08 -0500

    feat: text input auto-focus

--- framework/components/AContextualNotification/AContextualNotification.spec.js
+++ framework/components/AContextualNotification/AContextualNotification.spec.js
@@ -1,11 +1,21 @@
 context("ATextInput", () => {
   before(() => {
     cy.visit(
-      "http://localhost:8081/iframe.html?id=components-context--usage-1&viewMode=docs"
+      "http://localhost:8081/iframe.html?id=components-contextual-notifications--usage-1&viewMode=docs"
     );
   });
 
   it("supports themes", () => {
-    cy.matchImageSnapshot("a-context--theme");
+    // There isn't a way yet to tell if fonts are loaded, so wait 3 seconds.
+    cy.wait(3000)
+      .get("#story--components-contextual-notifications--usage-1")
+      .parent()
+      .parent()
+      .matchImageSnapshot("a-contextual-notification--usage-1");
+
+    cy.get("#story--components-contextual-notifications--dusk-1")
+      .parent()
+      .parent()
+      .matchImageSnapshot("a-contextual-notification--dusk-1");
   });
 });
