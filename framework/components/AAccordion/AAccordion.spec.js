929bf1fc7da79556dfcd3948801a8aae67cb4509
Anton Frattaroli <frattaro@gmail.com>
2020-05-05 09:53:57 -0500

    fix: remove accordion body max height (#124)

---
+++ framework/components/AAccordion/AAccordion.spec.js
@@ -0,0 +1,69 @@
+context("AAccordion", () => {
+  before(() => {
+    cy.visit(
+      "http://localhost:8081/iframe.html?id=components-accordions--usage-1&viewMode=docs"
+    );
+  });
+
+  it("tabs appropriately", () => {
+    cy.get("#story--components-accordions--usage-1 .a-button")
+      .eq(0)
+      .focus()
+      .tab();
+    cy.get("#story--components-accordions--usage-1 .a-accordion__link")
+      .eq(1)
+      .then(($el) => {
+        Cypress.dom.isFocused($el);
+      })
+      .tab();
+    cy.get("#story--components-accordions--usage-1 .a-accordion__link")
+      .eq(2)
+      .then(($el) => {
+        Cypress.dom.isFocused($el);
+      })
+      .tab();
+    cy.get("#story--components-accordions--usage-1 .a-button")
+      .eq(1)
+      .then(($el) => {
+        Cypress.dom.isFocused($el);
+      });
+  });
+
+  it("expands/collapses appropriately", () => {
+    cy.get("#story--components-accordions--usage-1 .a-accordion__card")
+      .eq(1)
+      .should("have.attr", "aria-expanded", "true")
+      .find(".a-accordion__link")
+      .should("have.attr", "role", "button")
+      .click();
+    cy.get("#story--components-accordions--usage-1 .a-accordion__card")
+      .eq(1)
+      .should("have.attr", "aria-expanded", "false")
+      .find(".a-accordion__link")
+      .type("{enter}");
+    cy.get("#story--components-accordions--usage-1 .a-accordion__card")
+      .eq(1)
+      .find(".a-accordion__link")
+      .type(" ");
+    cy.get("#story--components-accordions--usage-1 .a-accordion__card")
+      .eq(1)
+      .find(".a-accordion__link")
+      .type(" ");
+  });
+
+  it("supports themes", () => {
+    if (Cypress.env("snapshots") === "off") return;
+
+    // There isn't a way yet to tell if fonts are loaded, so wait 3 seconds.
+    cy.wait(3000)
+      .get("#story--components-accordions--usage-1")
+      .parent()
+      .parent()
+      .matchImageSnapshot("a-accordion--usage-1");
+
+    cy.get("#story--components-accordions--dusk-1")
+      .parent()
+      .parent()
+      .matchImageSnapshot("a-accordion--dusk-1");
+  });
+});
