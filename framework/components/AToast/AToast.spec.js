7406a57ecb6ddbdd6aebda26b6781ef3781debf5
Anton Frattaroli (afrattar) <afrattar@cisco.com>
2020-08-26 11:36:57 -0500

    feat: add toast component

---
+++ framework/components/AToast/AToast.spec.js
@@ -0,0 +1,25 @@
+context("AToast", () => {
+  before(() => {
+    cy.visitInLightTheme(
+      "http://localhost:8081/iframe.html?id=components-toasts--usage-1&viewMode=docs"
+    );
+  });
+
+  // TODO: Test interactability
+
+  // TODO: Test accessibility
+
+  it("supports themes", () => {
+    cy.get("#story--components-toasts--usage-1")
+      .parent()
+      .parent()
+      .matchImageSnapshot("a-toast--usage-1");
+
+    cy.get(".a-button").eq(1).click();
+
+    cy.get("#story--components-toasts--usage-1")
+      .parent()
+      .parent()
+      .matchImageSnapshot("a-toast--dusk-1");
+  });
+});
