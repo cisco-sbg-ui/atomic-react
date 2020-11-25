b26f77a68e1b4b0d57d24731c2c04be68d75a155
Anton Frattaroli <frattaro@gmail.com>
2020-11-25 18:37:41 -0500

    feat: add breadcrumb component (#306)

---
+++ framework/components/ABreadcrumb/ABreadcrumb.spec.js
@@ -0,0 +1,15 @@
+context("ABreadcrumb", () => {
+  before(() => {
+    cy.visitInLightTheme("http://localhost:8081/components/breadcrumb");
+  });
+
+  it("supports themes", () => {
+    if (Cypress.env("snapshots") === "off") return;
+
+    cy.get("#usage + .playground .playground__preview").toMatchImageSnapshot();
+
+    cy.get(".a-switch__box").eq(0).click();
+
+    cy.get("#usage + .playground .playground__preview").toMatchImageSnapshot();
+  });
+});
