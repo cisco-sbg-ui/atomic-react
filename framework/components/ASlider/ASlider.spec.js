7099cc5ef3fcc366b9f41147ae509b50b2875fd7
Anton Frattaroli (afrattar) <afrattar@cisco.com>
2020-12-10 01:33:13 -0600

    add changelog entry and coverage

--- framework/components/ASlider/ASlider.spec.js
+++ framework/components/ASlider/ASlider.spec.js
@@ -3,7 +3,43 @@ context("ASlider", () => {
     cy.visitInLightTheme("http://localhost:8081/components/slider");
   });
 
-  // TODO: Test interactability
+  it("has working keyboard controls", () => {
+    cy.get("#usage + .playground").should("contain", "Value: 2");
+    cy.get("#usage + .playground .a-slider__thumb").click().type("{upArrow}");
+    cy.get("#usage + .playground").should("contain", "Value: 3");
+    cy.get("#usage + .playground .a-slider__thumb").click().type("{downArrow}");
+    cy.get("#usage + .playground").should("contain", "You must agree");
+    cy.get("#usage + .playground .a-slider__thumb").click().type("{pageUp}");
+    cy.get("#usage + .playground .a-slider__thumb").click().type("{leftArrow}");
+    cy.get("#usage + .playground").should("contain", "Value: 4");
+    cy.get("#usage + .playground .a-slider__thumb").click().type("{pageDown}");
+    cy.get("#usage + .playground .a-slider__thumb")
+      .click()
+      .type("{rightArrow}");
+    cy.get("#usage + .playground .a-slider__thumb")
+      .click()
+      .type("{rightArrow}");
+    cy.get("#usage + .playground .a-slider__thumb")
+      .click()
+      .type("{rightArrow}");
+    cy.get("#usage + .playground").should("contain", "Value: 3");
+
+    cy.get("#range + .playground").should("contain", "Value: 0,1000");
+    cy.get("#range + .playground .a-slider__thumb").click().type("{upArrow}");
+    cy.get("#range + .playground").should("contain", "Value: 15,1000");
+    cy.get("#range + .playground .a-slider__thumb").click().type("{downArrow}");
+    cy.get("#range + .playground").should("contain", "Value: 0,1000");
+    cy.get("#range + .playground .a-slider__thumb").click().type("{pageUp}");
+    cy.get("#range + .playground").should("contain", "Value: 150,1000");
+    cy.get("#range + .playground .a-slider__thumb").click().type("{leftArrow}");
+    cy.get("#range + .playground").should("contain", "Value: 135,1000");
+    cy.get("#range + .playground .a-slider__thumb").click().type("{pageDown}");
+    cy.get("#range + .playground").should("contain", "Value: 0,1000");
+    cy.get("#range + .playground .a-slider__thumb")
+      .click()
+      .type("{rightArrow}");
+    cy.get("#range + .playground").should("contain", "Value: 15,1000");
+  });
 
   // TODO: Test accessibility
 
