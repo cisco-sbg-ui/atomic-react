357f5e998044c7370618b40010915317d8c8ed39
barvo <barvo@cisco.com>
2021-12-13 13:51:10 -0600

    feat: expandable data table rows
    refactor: update dependency array for data table items prop memoized calculations

--- framework/components/ADataTable/ADataTable.js
+++ framework/components/ADataTable/ADataTable.js
@@ -1,17 +1,39 @@
+import _ from "lodash";
 import PropTypes from "prop-types";
-import React, {forwardRef} from "react";
+import React, {forwardRef, useEffect, useMemo, useRef, useState} from "react";
 
 import AIcon from "../AIcon";
 import ASimpleTable from "../ASimpleTable";
 import "./ADataTable.scss";
 
-const ADataTable = forwardRef(
-  ({className: propsClassName, headers, items, onSort, sort, ...rest}, ref) => {
-    let className = "a-data-table";
+const TableHeader = (props) => <th role='columnheader' {...props} />;
+const TableRow = (props) => <tr role='row' {...props} />;
+const TableCell = (props) => <td role='cell' {...props} />;
+const uniqueRowId = Symbol('uuid');
 
+const ADataTable = forwardRef(
+  ({className: propsClassName, expandable, headers, items: propsItems, onSort, sort, ...rest}, ref) => {
+    const uniqueIdRef = useRef(_.uniqueId('a-data-table-'));
+    const [expandedRows, setExpandedRows] = useState({});
+    const uniqueTableId = uniqueIdRef.current;
+    const rowData = JSON.stringify(propsItems);
+    const ExpandableComponent = expandable?.component;
+    // Helps keep row state stable between renders (expansion, selection, etc.)
+    const items = useMemo(() => propsItems.map(i => ({...i, [uniqueRowId]: _.uniqueId(`${uniqueTableId}-`)}))
+    , [uniqueTableId, rowData]); // eslint-disable-line react-hooks/exhaustive-deps
+    let className = 'a-data-table';
+    if (ExpandableComponent) {
+      className += '--expandable'
+    }
     if (propsClassName) {
       className += ` ${propsClassName}`;
     }
+
+    const toggleRow = (uuid) => {
+      return () => {
+        setExpandedRows(prev => ({...prev, [uuid]: !prev[uuid]}))
+      }
+    };
 
     let sortedItems = items;
     if (sort) {
@@ -39,7 +61,8 @@ const ADataTable = forwardRef(
         <ASimpleTable {...rest} ref={ref} className={className}>
           {headers && (
             <thead>
-              <tr>
+              <TableRow>
+                {ExpandableComponent && <TableHeader><span>Toggle</span></TableHeader>}
                 {headers.map((x, i) => {
                   const headerProps = {
                     className: `a-data-table__header ${
@@ -88,7 +111,7 @@ const ADataTable = forwardRef(
                   }
 
                   return (
-                    <th {...headerProps} key={`a-data-table_header_${i}`}>
+                    <TableHeader {...headerProps} key={`a-data-table_header_${i}`}>
                       {x.align !== "end" && x.name}
                       {x.sortable && (
                         <AIcon
@@ -107,28 +130,63 @@ const ADataTable = forwardRef(
                         </AIcon>
                       )}
                       {x.align === "end" && x.name}
-                    </th>
+                    </TableHeader>
                   );
                 })}
-              </tr>
+                {ExpandableComponent && <TableHeader>Additional Details</TableHeader>}
+              </TableRow>
             </thead>
           )}
           <tbody>
-            {sortedItems.map((x, i) => (
-              <tr key={`a-data-table_row_${i}`}>
-                {headers.map((y, j) => (
-                  <td
-                    key={`a-data-table_cell_${j}`}
-                    className={`text-${y.align || "start"} ${
-                      y.cell?.className || ""
-                    }`.trim()}>
-                    {y.cell && y.cell.component
-                      ? y.cell.component(x)
-                      : x[y.key]}
-                  </td>
-                ))}
-              </tr>
-            ))}
+            {sortedItems.map((x) => {
+              const hasExpandedRowContent = ExpandableComponent &&
+                (typeof expandable.isRowExpandable === 'function'
+                  ? expandable.isRowExpandable(x)
+                  : true
+                );
+              const uuid = x[uniqueRowId];
+              return (
+                <TableRow data-expandable-row={hasExpandedRowContent} key={uuid} className='a-data-table__row'>
+                  {ExpandableComponent && (
+                    <TableCell>
+                      {hasExpandedRowContent && (
+                        <button
+                          aria-expanded={expandedRows[uuid]}
+                          aria-controls={uuid}
+                          className='a-data-table--expandable__cell__btn'
+                          onClick={toggleRow(uuid)}>
+                          <AIcon size={12}>
+                            {expandedRows[uuid] ? "chevron-down" : "chevron-right"}
+                          </AIcon>
+                        </button>
+                      )}
+                    </TableCell>
+                  )}
+                  {headers.map((y, j) => (
+                    <TableCell
+                      key={`a-data-table_cell_${j}`}
+                      className={`text-${y.align || "start"} ${
+                        y.cell?.className || ""
+                      }`.trim()}
+                      role='cell'>
+                      {y.cell && y.cell.component
+                        ? y.cell.component(x)
+                        : x[y.key]}
+                    </TableCell>
+                  ))}
+                  {hasExpandedRowContent && (
+                    <TableCell
+                      {..._.omit(expandable, ['component', 'isRowExpandable'])}
+                      id={uuid}
+                      data-expandable-content
+                      hidden={!expandedRows[uuid]}
+                      role='cell'>
+                      <ExpandableComponent {...x} />
+                    </TableCell>
+                  )}
+                </TableRow>
+              )
+            })}
           </tbody>
         </ASimpleTable>
       )
@@ -142,6 +200,18 @@ ADataTable.propTypes = {
    */
   altLinks: PropTypes.bool,
   /**
+   * Configuration object for expandable table rows
+   */
+  expandable: PropTypes.shape({
+    /** The component to be rendered on expansion */
+    component: PropTypes.func.isRequired,
+    /**
+     * (optional): A function called when rendering each row to determine
+     * if the row can be expanded an collapsed
+     */
+    isRowExpandable: PropTypes.func,
+  }),
+  /**
    * Sets the table headers.
    */
   headers: PropTypes.arrayOf(
@@ -154,7 +224,7 @@ ADataTable.propTypes = {
       className: PropTypes.string,
       /** The alignment of the header column text content */
       align: PropTypes.oneOf(["start", "center", "end"]),
-      /** Determinees if the column can be sorted by the user */
+      /** Determines if the column can be sorted by the user */
       sortable: PropTypes.bool,
       /** Sorting function for the column data */
       sort: PropTypes.oneOfType([PropTypes.func, PropTypes.bool]),
