3ca5678c46f73b5529efa5ab68233ac5d08ba627
barvo <barvo@cisco.com>
2022-02-03 09:56:04 -0600

    refactor: simplify date ranger sequencing logic

--- framework/components/ADatePicker/useADateRange.js
+++ framework/components/ADatePicker/useADateRange.js
@@ -1,22 +1,37 @@
-import { useState } from "react";
-  
-  const useADateRange = (initialRange  = [null, null]) => {
-    const [range, setRange] = useState(initialRange)
-  
-    const onChange = (date) => {
-      setRange(prevRange => {
-        const [prevStartDate, prevEndDate] = prevRange;
-        if (!prevStartDate && !prevEndDate) {
-          return [date, null];
-        }
-        return prevEndDate ? [date, null] : [prevStartDate, date];
-      })
-    }
-  
-    return {
-      value: range,
-      onChange,
-    };
-  }
+import { useCallback, useState } from "react";
 
-  export default useADateRange;
+/**
+ * Sequencing logic for setting an incoming date relative to an existing date range
+ * Flow: first date -> second date -> first date -> second date -> (etc.)
+ * @param {Array.<Date|null>} oldRange - Tuple with starting date and ending date
+ * @param {Date} incomingDate - The next date to sequence
+ * @returns tuple the newly set date range
+ */
+export const stepSequencer = (existingRange, nextDate) => {
+  const [startingDate, endingDate] = existingRange;
+  const isRangeEmpty = !startingDate && !endingDate;
+  const shouldResetRange = startingDate && endingDate;
+
+  return isRangeEmpty || shouldResetRange ?
+    [nextDate, null] :
+    [startingDate, nextDate];
+};
+
+/**
+ * Hook for storing date range state. Currently uses a step sequence, but has
+ * room for flexibility in the future.
+ */
+const useADateRange = (initialRange = [null, null]) => {
+  const [range, setRange] = useState(initialRange);
+
+  const onChange = useCallback((incomingDate) => {
+    setRange(oldRange => stepSequencer(oldRange, incomingDate));
+  }, [setRange]);
+
+  return {
+    value: range,
+    onChange,
+  };
+};
+
+export default useADateRange;
