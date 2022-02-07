343c01eabfd44e3db1100553bcd4dbfcd124f7d3
barvo <barvo@cisco.com>
2022-02-07 10:34:30 -0600

    refactor: add comments to explain date picker PropTypes for selecting a date range

--- framework/components/ADatePicker/ADatePicker.js
+++ framework/components/ADatePicker/ADatePicker.js
@@ -199,7 +199,13 @@ ADatePicker.propTypes = {
    */
   onChange: PropTypes.func,
   /**
-   * Sets the date(s) selected.
+   * Sets the selected date(s). If selecting a single date, a JavaScript
+   * `Date` object should be passed. If selecting a date range, however,
+   * then a two-item array should be supplied representing the starting
+   * date and ending date. If either the starting date or ending date is
+   * not yet selected, you should pass in the date as `null`.
+   * @example supplying the starting date with an unselected ending date
+   * value={[new Date(2022, 2, 28), null]}
    */
   value: PropTypes.oneOfType([
     PropTypes.instanceOf(Date),
