77614caec8b837add1c443466060549456edfdec
barvo <barvo@cisco.com>
2022-02-18 08:31:15 -0600

    feat: add React hook in utilities for listening to a media query in the document

--- framework/utils/hooks.js
+++ framework/utils/hooks.js
@@ -1,4 +1,4 @@
-import {useEffect, useLayoutEffect, useRef} from "react";
+import {useCallback, useEffect, useLayoutEffect, useRef, useState} from "react";
 
 export const useCombinedRefs = (...refs) => {
   const targetRef = useRef();
@@ -20,3 +20,40 @@ export const useCombinedRefs = (...refs) => {
 
 export const useIsomorphicLayoutEffect =
   typeof window !== "undefined" ? useLayoutEffect : useEffect;
+
+/**
+ * Uses the `matchMedia` API to dynamically determine if the document currently
+ * matches a particular media query.
+ * @param {string} mediaQuery - CSS media query to be tested the document
+ * @returns {boolean} indicates if the media
+ * query matches the document
+ * @see https://developer.mozilla.org/en-US/docs/Web/API/Window/matchMedia
+ */
+ export const useMediaQuery = (config) => {
+   const { mediaQuery, onChange } = config;
+  const mediaQueryList = useRef();
+  const [matches, setMatches] = useState(typeof window !== "undefined" && window?.matchMedia(mediaQuery)?.matches);
+
+  const validateQuery = useCallback((e) => {
+      if (typeof onChange === 'function') {
+          onChange(e);
+      }
+      setMatches(e?.target?.matches);
+  }, [
+      onChange,
+      setMatches
+  ]);
+
+  useEffect(() => {
+      mediaQueryList.current = window.matchMedia(mediaQuery);
+      setMatches(mediaQueryList.current.matches);
+      mediaQueryList.current.onchange = validateQuery;
+
+      return () => mediaQueryList.current.onchange = null;
+  }, [mediaQuery, validateQuery]);
+
+  return {
+    matches,
+    mediaQueryList: mediaQueryList.current,
+  };
+};
