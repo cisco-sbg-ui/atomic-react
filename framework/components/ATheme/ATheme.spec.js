ae755386d1e4041aa0f2f52ec574efe2b7cd0ebd
barvo <barvo@cisco.com>
2022-02-23 15:36:01 -0600

    chore: update ATheme spec file to properly select ATheme example components in cypress

--- framework/components/ATheme/ATheme.spec.js
+++ framework/components/ATheme/ATheme.spec.js
@@ -10,13 +10,13 @@ context("ATheme", () => {
   it("supports themes", () => {
     if (Cypress.env("snapshots") === "off") return;
 
-    cy.get("#usage + .playground .playground__preview").compareSnapshot(
+    cy.get("#usage ~ .playground .playground__preview").compareSnapshot(
       "Theme 1"
     );
 
-    cy.get("#usage + .playground .playground__preview .a-button").eq(1).click();
+    cy.get("#usage ~ .playground .playground__preview .a-button").eq(1).click();
 
-    cy.get("#usage + .playground .playground__preview").compareSnapshot(
+    cy.get("#usage ~ .playground .playground__preview").compareSnapshot(
       "Theme 2"
     );
   });
