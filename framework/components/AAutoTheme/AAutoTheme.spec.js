2592f2bc8979378407724483179fb24dc1e4f0f2
barvo <barvo@cisco.com>
2022-02-25 09:47:50 -0600

    chore: restructure AAutoTheme tests

--- framework/components/AAutoTheme/AAutoTheme.spec.js
+++ framework/components/AAutoTheme/AAutoTheme.spec.js
@@ -10,29 +10,33 @@ context("AAutoTheme", () => {
                     });
                 }
             })
-            .then(() => {
-                window.localStorage.setItem('atomic-react-auto-theme', "enabled");
-            });
     });
   
     it("supports automatic themes", () => {
       if (Cypress.env("snapshots") === "off") return;
   
       cy.get("#usage ~ .playground .playground__preview").eq(2).compareSnapshot(
-        "Theme 1"
+        "AutoTheme 1"
       );
   
-      cy.get("#usage ~ .playground .playground__preview .a-switch").eq(0).click();
-  
-      cy.get("#usage ~ .playground .playground__preview").eq(2).compareSnapshot(
-        "Theme 2"
-      );
-      
       cy.get("#usage ~ .playground .playground__preview .a-switch").eq(0).click();
 
       cy.get("#usage ~ .playground .playground__preview").eq(2).compareSnapshot(
-        "Theme 1"
+        "AutoTheme 2"
       );
+
+      cy.window().then((win) => {
+        cy.stub(win, "matchMedia")
+          .withArgs("(prefers-color-scheme: dark)")
+          .returns({
+            matches: false
+        });
+      })
+
+      cy.get("#usage ~ .playground .playground__preview").eq(2).compareSnapshot(
+        "AutoTheme 1"
+      );
+
     });
   });
   
