df6aba524582a433d3c3345ee9fb90e39e65ffd5
barvo <barvo@cisco.com>
2022-02-25 16:55:33 -0600

    refactor: update AAutoTheme system theme change test logic

--- framework/components/AAutoTheme/AAutoTheme.spec.js
+++ framework/components/AAutoTheme/AAutoTheme.spec.js
@@ -1,42 +1,27 @@
 context("AAutoTheme", () => {
-    before(() => {
-        cy
-            .visit("localhost:3000/components/theme", {
-                onBeforeLoad(win) {
-                cy.stub(win, "matchMedia")
-                    .withArgs("(prefers-color-scheme: dark)")
-                    .returns({
-                    matches: true
-                    });
-                }
-            })
-    });
-  
-    it("supports automatic themes", () => {
-      if (Cypress.env("snapshots") === "off") return;
-  
-      cy.get("#usage ~ .playground .playground__preview").eq(2).compareSnapshot(
-        "AutoTheme 1"
-      );
-  
-      cy.get("#usage ~ .playground .playground__preview .a-switch").eq(0).click();
+  it("persists automatic themes", () => {
+    if (Cypress.env("snapshots") === "off") return;
 
-      cy.get("#usage ~ .playground .playground__preview").eq(2).compareSnapshot(
-        "AutoTheme 2"
-      );
+    cy.visitInLightTheme("http://localhost:3000/components/theme");
 
-      cy.window().then((win) => {
+    cy.get(".root-sidebar .a-switch").compareSnapshot(
+      "AutoTheme 1"
+    );
+
+    cy.get(".root-sidebar .a-switch__box").eq(0).click();
+
+    cy.visit("localhost:3000/components/theme", {
+      onBeforeLoad(win) {
         cy.stub(win, "matchMedia")
-          .withArgs("(prefers-color-scheme: dark)")
-          .returns({
-            matches: false
-        });
-      })
-
-      cy.get("#usage ~ .playground .playground__preview").eq(2).compareSnapshot(
-        "AutoTheme 1"
-      );
-
+            .withArgs("(prefers-color-scheme: dark)")
+            .returns({
+              matches: true
+            });
+      }
     });
-  });
-  
+
+    cy.get(".root-sidebar .a-switch").compareSnapshot(
+      "AutoTheme 2"
+    );
+    });
+})
