88eaacd4644ae00856fdf0d9b27476bd77d09427
barvo <barvo@cisco.com>
2022-03-15 12:32:58 -0500

    util: add React hook for keeping count without causing re-renders

--- framework/utils/hooks.js
+++ framework/utils/hooks.js
@@ -18,6 +18,23 @@ export const useCombinedRefs = (...refs) => {
   return targetRef;
 };
 
+/**
+ * Keeps count within a component without
+ * causing re-renders. Helpful for internal
+ * logic that does not require an update to
+ * state.
+ */
+export const useCount = (initialCount = 0) => {
+  const count = useRef(initialCount);
+  return {
+    getCount: () => count.current,
+    setCount: (num) => (count.current = num),
+    increment: () => (count.current += 1),
+    decrement: () => (count.current -= 1),
+    resetCount: () => (count.current = 0)
+  };
+};
+
 export const useIsomorphicLayoutEffect =
   typeof window !== "undefined" ? useLayoutEffect : useEffect;
 
@@ -29,31 +46,33 @@ export const useIsomorphicLayoutEffect =
  * query matches the document
  * @see https://developer.mozilla.org/en-US/docs/Web/API/Window/matchMedia
  */
- export const useMediaQuery = (config) => {
-   const { mediaQuery, onChange } = config;
+export const useMediaQuery = (config) => {
+  const {mediaQuery, onChange} = config;
   const mediaQueryList = useRef();
-  const [matches, setMatches] = useState(typeof window !== "undefined" && window?.matchMedia(mediaQuery)?.matches);
+  const [matches, setMatches] = useState(
+    typeof window !== "undefined" && window?.matchMedia(mediaQuery)?.matches
+  );
 
-  const validateQuery = useCallback((e) => {
-      if (typeof onChange === 'function') {
-          onChange(e);
+  const validateQuery = useCallback(
+    (e) => {
+      if (typeof onChange === "function") {
+        onChange(e);
       }
       setMatches(e?.target?.matches);
-  }, [
-      onChange,
-      setMatches
-  ]);
+    },
+    [onChange, setMatches]
+  );
 
   useEffect(() => {
-      mediaQueryList.current = window.matchMedia(mediaQuery);
-      setMatches(mediaQueryList.current.matches);
-      mediaQueryList.current.onchange = validateQuery;
+    mediaQueryList.current = window.matchMedia(mediaQuery);
+    setMatches(mediaQueryList.current.matches);
+    mediaQueryList.current.onchange = validateQuery;
 
-      return () => mediaQueryList.current.onchange = null;
+    return () => (mediaQueryList.current.onchange = null);
   }, [mediaQuery, validateQuery]);
 
   return {
     matches,
-    mediaQueryList: mediaQueryList.current,
+    mediaQueryList: mediaQueryList.current
   };
 };
