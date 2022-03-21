a6b729ecf32025530d17ce345569278397270d0b
barvo <barvo@cisco.com>
2022-03-21 16:25:11 -0500

    chore: removed unused react hooks

--- framework/utils/hooks.js
+++ framework/utils/hooks.js
@@ -120,26 +120,6 @@ export const useIntersectionObserver = (cb, config = defaultConfig) => {
   return callbackRef;
 };
 
-/**
- * An implementation of the useIntersectionObserver
- * that is pre-configured for infinite scrolling in
- * a list.
- */
-export const useInfiniteScroll = (config) => {
-  const {onScroll, ...observerConfig} = config;
-  const scrollTriggerRef = useIntersectionObserver(({inView,entry}) => {
-    if (!inView) {
-      return;
-    }
-    onScroll(entry);
-  }, {
-    triggerOnce: true,
-    ...observerConfig,
-  });
-
-  return scrollTriggerRef;
-};
-
 export const useIsomorphicLayoutEffect =
   typeof window !== "undefined" ? useLayoutEffect : useEffect;
 
@@ -180,70 +160,4 @@ export const useMediaQuery = (config) => {
     matches,
     mediaQueryList: mediaQueryList.current
   };
-};
-
-/**
- * Tracks the presence of a DOM node
- * given configuration options supplied
- * to an Intersection Observer
- * @see https://developer.mozilla.org/en-US/docs/Web/API/IntersectionObserver/IntersectionObserver#parameters
- */
-export const useOnScreen = (config) => {
-  const {cache, onEnter, onExit, ...observerConfig} = config;
-  const o = useRef();
-  const intersectionCount = useCount();
-  const [isOnScreen, setIsOnScreen] = useState(
-    intersectionCount.getCount() > 0
-  );
-
-  const handleIntersection = (entry) => {
-    intersectionCount.increment();
-    setIsOnScreen(true);
-    if (typeof onEnter === "function") {
-      onEnter(entry);
-    }
-  };
-
-  const handleTargetExit = (entry) => {
-    setIsOnScreen(false);
-    if (typeof onExit === "function") {
-      onExit(entry);
-    }
-  };
-
-  const observer = useIntersectionObserver(
-    ([entry]) => {
-      const {isIntersecting, target} = entry;
-      const hasIntersectedOnce = intersectionCount.getCount() > 0;
-      if (cache && hasIntersectedOnce) {
-        observer.unobserve(target);
-        return;
-      }
-
-      isIntersecting ? handleIntersection(entry) : handleTargetExit(entry);
-    },
-    {...observerConfig}
-  );
-
-  const callbackRef = (incomingRef) => {
-    if (!incomingRef) {
-      observer.unobserve(o.current);
-      return;
-    }
-
-    o.current = incomingRef;
-    observer.observe(incomingRef);
-  };
-
-  const targetProps = {
-    ref: callbackRef
-  };
-
-  const target = {
-    onScreenCount: intersectionCount.getCount(),
-    isOnScreen,
-    getTargetProps: () => targetProps
-  };
-
-  return target;
 };
