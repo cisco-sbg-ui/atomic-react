5f99d0315e7acb79bd02d60205576e80a6cca5df
barvo <barvo@cisco.com>
2022-03-21 13:32:45 -0500

    test: update ADataTable example selectors with new documentation page

--- framework/components/ADataTable/ADataTable.spec.js
+++ framework/components/ADataTable/ADataTable.spec.js
@@ -3,49 +3,51 @@ context("ADataTable", () => {
     cy.visitInLightTheme("http://localhost:3000/components/data-table");
   });
 
+  const basicUsageSelector = `#basic + .playground`;
+
   it("sorts normally", () => {
-    cy.get("#usage + .playground td").eq(0).contains(11.1);
-    cy.get("#usage + .playground th").eq(0).click();
-    cy.get("#usage + .playground td").eq(0).contains(8.9);
-    cy.get("#usage + .playground th").eq(0).click();
-    cy.get("#usage + .playground td").eq(0).contains(11.1);
-    cy.get("#usage + .playground th .a-icon")
+    cy.get(`${basicUsageSelector} td`).eq(0).contains(11.1);
+    cy.get(`${basicUsageSelector} th`).eq(0).click();
+    cy.get(`${basicUsageSelector} td`).eq(0).contains(8.9);
+    cy.get(`${basicUsageSelector} th`).eq(0).click();
+    cy.get(`${basicUsageSelector} td`).eq(0).contains(11.1);
+    cy.get(`${basicUsageSelector} th .a-icon`)
       .eq(0)
       .should("have.attr", "aria-label", "chevron-down icon");
-    cy.get("#usage + .playground th").eq(0).click();
-    cy.get("#usage + .playground td").eq(0).contains(11.1);
+    cy.get(`${basicUsageSelector} th`).eq(0).click();
+    cy.get(`${basicUsageSelector} td`).eq(0).contains(11.1);
   });
 
   it("allows disabled sort", () => {
-    cy.get("#usage + .playground td").eq(1).contains(526);
-    cy.get("#usage + .playground th").eq(1).click();
-    cy.get("#usage + .playground td").eq(1).contains(526);
-    cy.get("#usage + .playground th").eq(1).click();
-    cy.get("#usage + .playground td").eq(1).contains(526);
-    cy.get("#usage + .playground th .a-icon")
+    cy.get(`${basicUsageSelector} td`).eq(1).contains(526);
+    cy.get(`${basicUsageSelector} th`).eq(1).click();
+    cy.get(`${basicUsageSelector} td`).eq(1).contains(526);
+    cy.get(`${basicUsageSelector} th`).eq(1).click();
+    cy.get(`${basicUsageSelector} td`).eq(1).contains(526);
+    cy.get(`${basicUsageSelector} th .a-icon`)
       .eq(1)
       .should("have.attr", "aria-label", "chevron-down icon");
-    cy.get("#usage + .playground th").eq(1).click();
-    cy.get("#usage + .playground td").eq(1).contains(526);
+    cy.get(`${basicUsageSelector} th`).eq(1).click();
+    cy.get(`${basicUsageSelector} td`).eq(1).contains(526);
   });
 
   it("allows custom sort", () => {
-    cy.get("#usage + .playground td").eq(2).contains("aardvark");
-    cy.get("#usage + .playground th").eq(2).click();
-    cy.get("#usage + .playground td").eq(2).contains("aardvark");
-    cy.get("#usage + .playground th").eq(2).click();
-    cy.get("#usage + .playground td").eq(2).contains("Zanzibar");
-    cy.get("#usage + .playground th .a-icon")
+    cy.get(`${basicUsageSelector} td`).eq(2).contains("aardvark");
+    cy.get(`${basicUsageSelector} th`).eq(2).click();
+    cy.get(`${basicUsageSelector} td`).eq(2).contains("aardvark");
+    cy.get(`${basicUsageSelector} th`).eq(2).click();
+    cy.get(`${basicUsageSelector} td`).eq(2).contains("Zanzibar");
+    cy.get(`${basicUsageSelector} th .a-icon`)
       .eq(2)
       .should("have.attr", "aria-label", "chevron-down icon");
-    cy.get("#usage + .playground th").eq(2).click();
-    cy.get("#usage + .playground td").eq(2).contains("aardvark");
+    cy.get(`${basicUsageSelector} th`).eq(2).click();
+    cy.get(`${basicUsageSelector} td`).eq(2).contains("aardvark");
   });
 
   it("aligns correctly", () => {
     if (Cypress.env("snapshots") === "off") return;
 
-    cy.get("#usage + .playground .playground__preview").compareSnapshot(
+    cy.get(`${basicUsageSelector} .playground__preview`).compareSnapshot(
       "DataTable 1"
     );
   });
