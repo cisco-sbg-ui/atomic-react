9a86b99f0cd8c1513d57357c84f83e342f317e73
barvo <barvo@cisco.com>
2022-04-13 14:06:08 -0500

    test: add support for Cypress component testing

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -1,6 +1,12 @@
 import "@cypress/code-coverage/support";
+import { mount as cypressMount } from '@cypress/react';
+import AApp from "../../framework/components/AApp";
 
 import "./commands";
+
+Cypress.Commands.add('mount', (children) => {
+  return cypressMount(<AApp>{children}</AApp>);
+});
 
 require("cypress-plugin-tab");
 
