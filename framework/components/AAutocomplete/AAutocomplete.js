ec774afa371ada12e8865fa29203341de4014c81
Rui Wang <rwang3@cisco.com>
2022-11-07 15:13:05 -0800

    enable menu open on focus

--- framework/components/AAutocomplete/AAutocomplete.js
+++ framework/components/AAutocomplete/AAutocomplete.js
@@ -180,6 +180,7 @@ const AAutocomplete = forwardRef(
       },
       onFocus: () => {
         setIsFocused(true);
+        setIsOpen(true);
       },
       onKeyDown: (e) => {
         if (e.keyCode === keyCodes.up) {
@@ -286,6 +287,10 @@ AAutocomplete.propTypes = {
    * Toggles whether to display a clearable icon.
    */
   clearable: PropTypes.bool,
+  /***
+   * allow initial menu open when on focus
+   */
+  showMenuOnFocus: PropTypes.bool,
   /**
    * Toggles the disabled state.
    */
